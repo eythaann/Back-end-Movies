57390d3dd940a2652eca4696f22778305b12ecc9
Eythan David <eythan.cvt@gmail.com>
2021-12-04 16:09:24 -0500

    feat: post routes

---
+++ src/interfaces/actor.interface.ts
@@ -0,0 +1,9 @@
+export default interface actor {
+  name: string;
+  lastname: string;
+  biography: string;
+  born: string;
+  death: string;
+  place: string;
+  img: string;
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,9 @@
 import { Application, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 import multer from "multer";
 import pool from "../database";
 import movie from "../interfaces/movie.interface";
-import { body, validationResult } from "express-validator";
+
 const storage = multer.diskStorage({
   destination: "public/assets/img",
   filename: (req, file, cb) => {
@@ -24,16 +25,16 @@ export const loadApiEndpoints = (app: Application): void => {
 
   app.post(
     "/movies",
+    upload.single("image"),
     [
-      body("title").isLength({ min: 1 }),
-      body("description").isLength({ min: 1 }),
-      body("trailerLink").isLength({ min: 1 }),
-      body("premiere").isLength({ min: 1 }),
+      body("title", "Empty").isLength({ min: 1 }),
+      body("description", "Min 30 Characters").isLength({ min: 30 }),
+      body("trailerLink", "Not is a URL").isURL(),
+      body("premiere", "Not is a Date").isDate(),
     ],
-    upload.single("coverPage"),
     (req: Request, res: Response) => {
       const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.json(errors);
+      if (!errors.isEmpty()) return res.status(400).json(errors);
 
       const movie: movie = {
         title: req.body.title,
@@ -43,9 +44,9 @@ export const loadApiEndpoints = (app: Application): void => {
         img: req.file?.path || "no-image",
       };
 
-      pool.query(`INSERT INTO movies SET ?`, [movie], (err, result, fields) => {
+      pool.query(`INSERT INTO movies SET ?`, [movie], (err, result) => {
         if (err) throw err;
-        return res.json({ id: result.insertId });
+        return res.status(201).json({ id: result.insertId });
       });
     }
   );
@@ -54,9 +55,17 @@ export const loadApiEndpoints = (app: Application): void => {
     const movie = req.params.id;
     pool.query(`SELECT * FROM movies WHERE id=${movie}`, (err, result) => {
       if (err) throw err;
-      return result.length === 0
-        ? res.status(404).json({ error: "Not Exist" })
-        : res.status(200).json(result);
+      if (result.length === 0)
+        return res.status(404).json({ error: "Not Exist" });
+
+      pool.query(
+        `Select actorsid, interpretation FROM casts where moviesid = ${movie}`,
+        (err, actors) => {
+          if (err) throw err;
+          result[0].cast = actors;
+          return res.status(200).json(result[0]);
+        }
+      );
     });
   });
 
@@ -71,9 +80,48 @@ export const loadApiEndpoints = (app: Application): void => {
     const actor = req.params.id;
     pool.query(`SELECT * FROM actors WHERE id=${actor}`, (err, result) => {
       if (err) throw err;
-      return result.length === 0
-        ? res.status(404).json({ error: "Not Exist" })
-        : res.status(200).json(result);
+      if (result.length === 0)
+        return res.status(404).json({ error: "Not Exist" });
+
+      pool.query(
+        `Select moviesid, interpretation FROM casts where actorsid = ${actor}`,
+        (err, movies) => {
+          if (err) throw err;
+          result[0].interpretations = movies;
+          return res.status(200).json(result[0]);
+        }
+      );
     });
   });
+
+  app.post(
+    "/actor",
+    upload.single("image"),
+    [
+      body("name", "Min 2 Characters,max30").isLength({ min: 2, max: 30 }),
+      body("lastname", "Min 2 Characters,max30").isLength({ min: 2, max: 30 }),
+      body("biography", "Min 30 Max 500").isLength({ min: 30, max: 500 }),
+      body("born", "Not is a Date").isDate(),
+      body("place", "Min 2 Characters, max 50").isLength({ min: 2, max: 50 }),
+    ],
+    (req: Request, res: Response) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.status(400).json(errors);
+
+      const actor = {
+        name: req.body.name,
+        lastname: req.body.lastname,
+        biography: req.body.biography,
+        born: req.body.born,
+        death: req.body.death,
+        place: req.body.place,
+        img: req.file?.path || "no-image",
+      };
+
+      pool.query(`INSERT INTO actors SET ?`, [actor], (err, result) => {
+        if (err) throw err;
+        return res.status(201).json({ id: result.insertId });
+      });
+    }
+  );
 };
